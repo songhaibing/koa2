3b094361feb59c01ce1eeabce062c9462cfd73a8
shb <2269629396@qq.com>
2018-05-08 18:01:27 +0800

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
